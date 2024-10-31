c1e6c15109a281b4eb2b5c3f0a837436980bd9fa
Deni Fahrony <dfahrony7@gmail.com>
2024-10-31 21:41:53 +0700

    init routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 // setting up dependencies
 const express = require('express')
+const routes = require('./routes/routes')
 const { connectDB } = require('./database/postgres')
 
 const app = express()
@@ -7,6 +8,8 @@ const port = 3000
 
 // connect database
 connectDB()
+
+app.use('/api', routes)
 
 app.listen(port, () => {
     console.log('server run on port ', port)
