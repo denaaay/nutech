f244c50b5edc145af8206ae383773264cd0ece2c
Deni Fahrony <dfahrony7@gmail.com>
2024-10-31 21:27:24 +0700

    init and add connection to database postgres

---
+++ backend/src/database/postgres.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const { Sequelize } = require('sequelize')
+
+// get environment
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        logging: false,
+    }
+)
+
+// connect database
+const connectDB = async () => {
+    try {
+        await sequelize.authenticate()
+        console.log('database connected')
+    } catch (e) {
+        console.error('error connecting database: ', e)
+    }
+}
+
+module.exports = {
+    sequelize,
+    connectDB
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,12 @@
 // setting up dependencies
 const express = require('express')
+const { connectDB } = require('./database/postgres')
 
 const app = express()
 const port = 3000
+
+// connect database
+connectDB()
 
 app.listen(port, () => {
     console.log('server run on port ', port)
